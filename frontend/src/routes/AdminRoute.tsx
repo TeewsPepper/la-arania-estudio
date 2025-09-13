0ca0bdc96b4c48ec4ba3d605145cd77c5fe6cfdc
TeewsPepper <gomez.pepper@gmail.com>
2025-09-13 06:52:56 -0300

    update config

--- frontend/src/routes/AdminRoute.tsx
+++ frontend/src/routes/AdminRoute.tsx
@@ -2,9 +2,11 @@ import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 export const AdminRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  if (user?.role !== "admin") {
+  if (loading) return <p>Cargando usuario...</p>;
+
+  if (!user || user.role !== "admin") {
     return <Navigate to="/" replace />;
   }
 
--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,29 +1,4 @@
-/* 
-import type { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
 
-interface ProtectedRouteProps {
-  children: ReactNode;
-  adminOnly?: boolean; // opcional
-}
-
-export function ProtectedRoute({ children, adminOnly = false }: ProtectedRouteProps) {
-  const { user } = useAuth();
-
-  if (!user) {
-    // No autenticado → redirige a login
-    return <Navigate to="/login" replace />;
-  }
-
-  if (adminOnly && user.role !== "admin") {
-    // Usuario no es admin → redirige a perfil
-    return <Navigate to="/perfil" replace />;
-  }
-
-  // Usuario autorizado
-  return <>{children}</>;
-} */
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
