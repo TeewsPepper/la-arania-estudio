5b69b959c58900d4ebf0e338bed49f74402c4c23
TeewsPepper <gomez.pepper@gmail.com>
2025-09-18 12:41:34 -0300

    update config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -51,7 +51,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    domain: process.env.NODE_ENV === "production" ? ".araniauy.com" : undefined,
+    
   }
 }));
 
